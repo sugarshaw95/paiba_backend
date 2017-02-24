09c26681858f25c46fa870cb3c388eff2f669232
tcy_ghwjaong <wzy960724@gmail.com>
2017-02-24 18:20:06 +0800

    Fix.

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ NOW=$(date +"%m-%d-%Y-%H-%M")
 echo configure nginx
 sudo cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old
 sudo cp ./nginx.conf /etc/nginx/nginx.conf
-sudo sed -i "s/PATH/$script_dir/g" /etc/nginx/nginx.conf
+sudo sed -ie 's/PATH/$script_dir/g' /etc/nginx/nginx.conf
 sudo cat /etc/nginx/nginx.conf
 
 echo generate DH Parameters
@@ -20,10 +20,10 @@ sudo openssl dhparam 2048 -out /etc/nginx/cert/dhparam.pem
 sudo certbot certonly --agree-tos --webroot -w $script_dir -d 123.207.126.160
 
 echo configure gunicorn
-sudo cp ./gumicorn-paiba.service /etc/systemd/system/gumicorn-paiba.service
-sudo sed -i "s/PATH/$script_dir/g" /etc/systemd/system/gumicorn-paiba.service
+sudo cp ./gumicorn-paiba.service /etc/systemd/system/gunicorn-paiba.service
+sudo sed -ie 's/PATH/$script_dir/g' /etc/systemd/system/gunicorn-paiba.service
 
-sudo systemctl start gumicorn-paiba
-sudo systemctl enable gumicorn-paiba
+sudo systemctl start gunicorn-paiba
+sudo systemctl enable gunicorn-paiba
 sudo nginx -t
 sudo systemctl restart nginx
