875ad26bb0fe28dbc280858734d8fb154d439675
tcy_ghwjaong <wzy960724@gmail.com>
2017-03-04 15:18:27 +0800

    Remove security configs.

--- deploy.sh
+++ deploy.sh
@@ -13,11 +13,6 @@ sudo cp ./nginx.conf /etc/nginx/nginx.conf
 sudo sed -i "s,PATH,${script_dir},g" /etc/nginx/nginx.conf
 sudo cat /etc/nginx/nginx.conf
 
-echo generate DH Parameters
-sudo mkdir -p /etc/nginx/cert/
-sudo openssl dhparam 2048 -out /etc/nginx/cert/dhparam.pem
-sudo certbot certonly --agree-tos --webroot -w $script_dir -d 123.207.126.160
-
 echo configure gunicorn
 sudo cp ./gunicorn-paiba.service /etc/systemd/system/gunicorn-paiba.service
 sudo sed -i "s,PATH,${script_dir},g" /etc/systemd/system/gunicorn-paiba.service
