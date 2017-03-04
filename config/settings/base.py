a9aff99747defd76a11387807ebd8287e0731afb
tcy_ghwjaong <wzy960724@gmail.com>
2017-03-04 16:57:31 +0800

    Use the correct static file root.

--- config/settings/base.py
+++ config/settings/base.py
@@ -183,7 +183,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # STATIC FILE CONFIGURATION
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = str(ROOT_DIR.path('staticfiles'))
+STATIC_ROOT = str(APPS_DIR.path('static'))
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
